e95fd449edab9078cc40e519269068bcca759170
Mike Arthur <mike@mikearthur.co.uk>
2009-02-21 17:05:49 +0000

    Fix 404 validation.

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
 <?php get_sidebar(); ?>
 
 <h2>SORRY CANT FIND YOUR PAGE</h2>
-<img src="<?php bloginfo('template_directory'); ?>/images/404monkey.jpg" alt="PAGE FETCHING MONKEY">
+<img src="<?php bloginfo('template_directory'); ?>/images/404monkey.jpg" alt="PAGE FETCHING MONKEY" />
 <h3>MONKEY LOOK VERY HARD! CANNOT FIND YOUR PAGE?</h3>
 <h3>WHERE IS THE PAGE?</h3>
 <h3>YOU TYPE WRONG IN ADDRESS BAR?</h3>
