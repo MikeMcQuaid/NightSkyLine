463349fc4cf3d581ede38bbe7d6862eaa24a7567
Mike Arthur <mike@mikearthur.co.uk>
2009-10-07 22:55:51 +0100

    Move script to bottom of page to improve page load times.

--- footer.php
+++ footer.php
@@ -1,6 +1,3 @@
-<script type="text/javascript" src="http://twitter.com/javascripts/blogger.js"></script>
-<script type="text/javascript" src="http://twitter.com/statuses/user_timeline/mikearthur.json?callback=twitterCallback2&amp;count=5"></script>
-
 </div>
 
 <div id="footericons">
@@ -19,6 +16,8 @@
 London header image &copy; <a href="http://www.flickr.com/photos/peter_schauer/" rel="external">Peter Schauer</a>.
 <br/>
 All posts and pages licensed under <a href="http://creativecommons.org/licenses/by-nc-sa/2.0/uk/">Creative Commons Attribution-Non-Commercial-Share Alike</a>
+<script type="text/javascript" src="http://twitter.com/javascripts/blogger.js"></script>
+<script type="text/javascript" src="http://twitter.com/statuses/user_timeline/mikearthur.json?callback=twitterCallback2&amp;count=5"></script>
 <?php wp_footer(); ?>
 </div>
 </div>
