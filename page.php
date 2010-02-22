d1f5f412bad640460f0e1600df857e2d6713531f
Mike Arthur <mike@mikearthur.co.uk>
2010-02-22 21:12:44 +0000

    Don't append post titles with "Private" or "Protected".

--- page.php
+++ page.php
@@ -5,7 +5,7 @@
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<h2 class="storytitle"><a href="<?php the_permalink() ?>"
-rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+rel="bookmark" title="<?php echo $post->post_title; ?>"><?php echo $post->post_title; ?></a></h2>
 		<?php the_content(__('(more...)')); ?>
          <?php wp_link_pages(); ?>
 
