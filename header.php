da3499169df706fc3e20f3347730d4a99f32dd9b
Mike McQuaid <mike@mikemcquaid.com>
2010-07-30 21:15:32 +0100

    W3C accessibility fixes.

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes();?>>
   <head profile="http://gmpg.org/xfn/11">
     <?php if (have_posts()) the_post(); rewind_posts(); ?>
     <title><?php wp_title("");if(wp_title("", false)){echo ' - ';}bloginfo('name');?></title>
@@ -12,13 +12,14 @@
           href="<?php bloginfo('stylesheet_url'); ?>" media="screen" />
     <link rel="icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.gif" type="image/gif"/>
     <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?>" href="/feed/" />
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
     <?php wp_head(); ?>
   </head>
 <body>
 <div id="page">
 <div id="title">
   <h1><?php bloginfo('name'); ?></h1>
-  <p id="description"><?php bloginfo('description'); ?></p>
+  <p id="tagline"><?php bloginfo('description'); ?></p>
 </div>
 
 <div id="navbar">
