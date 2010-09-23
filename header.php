b19e1378681852492e1f7d39009e9f6956cb37eb
Mike McQuaid <mike@mikemcquaid.com>
2010-09-23 22:43:12 +0100

    Add initial HTML5 support.

--- header.php
+++ header.php
@@ -1,7 +1,6 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes();?>>
-  <head profile="http://gmpg.org/xfn/11">
+  <head>
     <?php if (have_posts()) the_post(); rewind_posts(); ?>
     <title><?php wp_title("");if(wp_title("", false)){echo ' - ';}bloginfo('name');?></title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
@@ -13,6 +12,7 @@
     <link rel="icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.gif" type="image/gif"/>
     <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?>" href="/feed/" />
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
     <?php wp_head(); ?>
   </head>
 <body>
