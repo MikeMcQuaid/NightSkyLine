4fa7c82719da429f90569813372e3a62a969da84
Mike McQuaid <mike@mikemcquaid.com>
2011-05-19 13:24:15 +0100

    Remove test html as minifying now.

--- get_and_fix_test_html.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Downloads the test.html file and fixes up bad URLs.
-
-rm test.html
-wget mikemcquaid.com -O test.html
-sed -i 's/http:\/\/mikemcquaid.com\/wp-content\/themes\/nightskyline//g' test.html
-sed -i 's/href="\//href="/g' test.html
