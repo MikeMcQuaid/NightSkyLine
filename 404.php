90664a3bf1ebd64d1f8d18c7ddee9980bad64485
Mike McQuaid <mike@mikemcquaid.com>
2011-12-05 22:29:46 +0000

    Fix 404 page.

--- 404.php
+++ 404.php
@@ -1,15 +1,15 @@
 <?php get_header(); ?>
 
 <div id="posts">
-    <div class="post">
-        <h2>SORRY CANT FIND YOUR PAGE</h2>
-        <img src="<?php bloginfo('template_directory'); ?>/images/404monkey.jpg" alt="404 MONKEY" />
-        <h3>MONKEY LOOK VERY HARD! CANNOT FIND YOUR PAGE?</h3>
-        <h3>WHERE IS THE PAGE?</h3>
-        <h3>YOU TYPE WRONG IN ADDRESS BAR?</h3>
-        <h3>TRY SEARCH INSTEAD, MAYBE FIND PAGE?</h3>
+    <div class="type-page">
+        <h2 class="storytitle">SORRY CANT FIND YOUR PAGE</h2>
+        <p><img src="<?php bloginfo('template_directory'); ?>/images/404monkey.jpg" alt="404 MONKEY" /></p>
+        <p>MONKEY LOOK VERY HARD! CANNOT FIND YOUR PAGE?</p>
+        <p>WHERE IS THE PAGE?</p>
+        <p>YOU TYPE WRONG IN ADDRESS BAR?</p>
+        <p>TRY SEARCH INSTEAD, MAYBE FIND PAGE?</p>
     </div>
-    <?php get_sidebar("footer"); ?>
 </div>
 
+<?php get_sidebar("footer"); ?>
 <?php get_footer(); ?>
