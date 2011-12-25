5bbd4ca4252dcd6e9d075105ddf75f29a9108267
Mike McQuaid <mike@mikemcquaid.com>
2011-12-25 17:10:07 +0000

    Don't use separate template file for pages.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <div id="posts">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <div <?php post_class(); ?>>
             <h2 class="storytitle">
                 <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>">
                     <?php the_title(); ?>
