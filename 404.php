73a76648b7dfd7dd812dccb9024cdedbc9530b99
Mike McQuaid <mike@mikemcquaid.com>
2011-12-31 14:40:31 +0000

    Make 404 monkey labelling clearer.

--- 404.php
+++ 404.php
@@ -2,9 +2,9 @@
 
 <div id="posts">
     <div class="type-page">
-        <h2 class="storytitle">SORRY NO FIND YOUR PAGE (404)</h2>
+        <h2 class="storytitle">404 MONKEY</h2>
         <p><img src="<?php bloginfo("template_directory"); ?>/images/404monkey.jpg" alt="404 MONKEY" /></p>
-        <p>404 MONKEY LOOK VERY HARD! NO FIND YOUR PAGE?</p>
+        <p>ME LOOK VERY HARD! NO FIND YOUR PAGE?</p>
         <p>WHERE IS THE PAGE?</p>
         <p>YOU TYPE WRONG IN ADDRESS BAR?</p>
         <p>TRY SEARCH INSTEAD, MAYBE FIND PAGE?</p>
