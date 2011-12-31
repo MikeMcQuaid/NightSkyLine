217bf8739a9377797910700bfdff41e6f0109617
Mike McQuaid <mike@mikemcquaid.com>
2011-12-31 13:24:49 +0000

    Tweak default footer.

--- sidebar-footer.php
+++ sidebar-footer.php
@@ -3,21 +3,16 @@
         <div class="sidebartext">
             <ul>
                 <?php if (!dynamic_sidebar("Footer Left")) { ?>
+                    <li><ul><?php wp_list_bookmarks(); ?></ul></li>
                     <li>
-                        <h2>Search</h2>
-                        <?php get_search_form(); ?>
-                    </li>
-
-                    <li>
-                        <h2>Categories</h2>
-                        <ul><?php wp_dropdown_categories(); ?></ul>
-                    </li>
-
-                    <li>
-                        <h2>Copyright</h2>
+                        <h2>Meta</h2>
+                        <?php wp_meta(); ?>
                         <ul>
-                            <li><a href="https://github.com/mikemcquaid/NightSkyLine/">NightSkyLine theme</a></li>
-                            <li>&copy; <a href="http://mikemcquaid.com/">Mike McQuaid</a></li>
+                            <li><a href="<?php bloginfo("rss_url"); ?>">RSS feed</a></li>
+                            <li><a href="https://github.com/mikemcquaid/NightSkyLine/">Using NightSkyLine theme</a></li>
+                            <li><a href="http://wordpress.org" rel="external">
+                                Powered by WordPress
+                            </a></li>
                         </ul>
                     </li>
                 <?php } ?>
@@ -29,16 +24,9 @@
         <div class="sidebartext">
             <ul>
                 <?php if (!dynamic_sidebar("Footer Right")) { ?>
-                    <li><ul><?php wp_list_bookmarks(); ?></ul></li>
                     <li>
-                        <h2>Meta</h2>
-                        <?php wp_meta(); ?>
-                        <ul>
-                            <li><a href="<?php bloginfo("rss_url"); ?>">RSS feed</a></li>
-                            <li><a href="http://wordpress.org" rel="external">
-                                Powered by WordPress
-                            </a></li>
-                        </ul>
+                        <h2>Search</h2>
+                        <?php get_search_form(); ?>
                     </li>
                 <?php } ?>
             </ul>
