86d55665cce72cb6c5448212fa884b0cc8c97366
Mike McQuaid <mike@mikemcquaid.com>
2012-01-02 13:29:06 +0000

    Image fixes.

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
 <div id="main">
 	<div id="content">
 		<div class="type-page">
-			<h2 class="entry_title"><img src="<?php bloginfo("template_directory"); ?>/images/404.jpg" alt="404 Manatee" /></h2>
+			<h2 class="entry_title"><img src="<?php bloginfo("template_directory"); ?>/images/404.jpg" alt="404 Manatee" height="400" width="400" /></h2>
 			<p>I'm terribly sorry; I've not been able to find the page you were looking for. Would you mind searching instead? Thanks so much!</p>
 			<?php get_search_form(); ?>
 		</div>
