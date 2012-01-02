b0f281915639378f32b184696a33467579740455
Mike McQuaid <mike@mikemcquaid.com>
2012-01-02 11:50:43 +0000

    Fix 404 image sizing on mobile.

--- 404.php
+++ 404.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <div id="posts">
-	<div id="pagenotfound">
+	<div class="type-page">
 		<h2 class="storytitle"><img src="<?php bloginfo("template_directory"); ?>/images/404.jpg" alt="404 Manatee" /></h2>
 		<p>I'm terribly sorry; I've not been able to find the page you were looking for. Would you mind searching instead? Thanks so much!</p>
 		<?php get_search_form(); ?>
